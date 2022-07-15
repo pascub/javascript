a158abf914bcbc332320b36677f102faff814eae
Renzo Bonavita <pascualbonavita1@gmail.com>
2022-07-14 21:00:58 -0300

    Primer entrega Proyecto Final

--- JS/index.js
+++ JS/index.js
@@ -44,7 +44,7 @@ class Producto{
 
 }
 let productos =[];
-
+            //   VENDER PRODUCTOS
 productos.push(new Producto("televisor", 10000));
 productos.push(new Producto("cafetera", 7000));
 productos.push(new Producto("tostadora", 5500));
@@ -54,5 +54,37 @@ for (const producto of productos){
     if(producto.precio === 10000){
     producto.vender()
     }
-}
+};
+
 console.log(productos);
+
+
+            // SUMAR TOTAL
+const carrito = [
+    { id: 1, nombre:"televisor", precio: 10000},
+    { id: 2, nombre:"cafetera", precio: 7000},
+    { id: 3, nombre:"tostadora", precio: 5500},
+    { id: 4, nombre:"microondas", precio: 8750}
+];
+let total = carrito.reduce ((acumulador, elemento) => + elemento.precio, 0);
+
+console.log(total);
+
+                // FILTRAR PRECIO
+
+let resultado = carrito.filter((el) => el.precio > 7000)
+console.log(resultado);
+
+
+        // ORDENAR PRECIO DE MENOR A MAYOR
+console.log(
+carrito.sort ((a, b) => {
+    if (a.precio > b.precio){
+        return 1;
+    }
+    if (a.precio < b.precio) {
+        return -1;
+    }
+    return 0;
+})
+)
